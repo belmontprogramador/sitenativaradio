010431ef958813672abd3de520afc48244a5c6c9
.git <belmontprogramador@gmail.com>
2025-10-13 19:23:05 -0300

    build: pronto para deploy no Railway

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"]
+    }
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
+};
+
+export default nextConfig;
